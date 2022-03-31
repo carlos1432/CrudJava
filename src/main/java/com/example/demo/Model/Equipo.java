1fb7d00440bd8bf29a58a32d5c02716154d65a69
Carlos Forero <candres.forero.09@gmail.com>
2022-03-31 12:06:36 -0500

    first commit

---
+++ src/main/java/com/example/demo/Model/Equipo.java
@@ -0,0 +1,12 @@
+package com.example.demo.Model;
+/**
+ *
+ * @author cforero
+ */
+public class Equipo {
+    int IdEquipo;
+    String nombre;
+    String pais;
+    String ciudad;
+    String numerodDeJugadores;
+}
---
+++ src/main/java/com/example/demo/Model/Jugador.java
@@ -0,0 +1,13 @@
+package com.example.demo.Model;
+
+/**
+ *
+ * @author cforero
+ */
+public class Jugador {
+    int IdJugador;
+    String nombre;
+    String apellido;
+    int edad;
+    String posicion;
+}
